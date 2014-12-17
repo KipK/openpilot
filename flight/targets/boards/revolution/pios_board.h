3709302f7eb0c5db19a5a47ee57033d638b4e72b
KipK <hotlobster75@gmail.com>
2014-12-17 13:01:35 +0100

    Added LightTelemetry (LTM) module for OpenPilot Revolution.
    This is a one way telemetry that can handle low bitrate like 1200 bauds. Perfect for audio modem or low baudrate RF link to maximise range.
    
    It was originally to work with Ghettostatation Antenna tracker & ground OSD.
    But there's some other client/GCS around that handles this protocol now (ie. MWPTools)
    
    You can configure baudrate on System/hwsettings window

--- flight/targets/boards/revolution/pios_board.h
+++ flight/targets/boards/revolution/pios_board.h
@@ -147,6 +147,7 @@ extern uint32_t pios_com_telem_usb_id;
 extern uint32_t pios_com_bridge_id;
 extern uint32_t pios_com_vcp_id;
 extern uint32_t pios_com_hkosd_id;
+extern uint32_t pios_com_lighttelemetry_id;
 #define PIOS_COM_GPS       (pios_com_gps_id)
 #define PIOS_COM_TELEM_USB (pios_com_telem_usb_id)
 #define PIOS_COM_TELEM_RF  (pios_com_telem_rf_id)
@@ -154,6 +155,7 @@ extern uint32_t pios_com_hkosd_id;
 #define PIOS_COM_BRIDGE    (pios_com_bridge_id)
 #define PIOS_COM_VCP       (pios_com_vcp_id)
 #define PIOS_COM_OSDHK     (pios_com_hkosd_id)
+#define PIOS_COM_LIGHTTELEMETRY (pios_com_lighttelemetry_id)
 
 #if defined(PIOS_INCLUDE_DEBUG_CONSOLE)
 extern uint32_t pios_com_debug_id;
